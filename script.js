439090a483d21ebe66b36fbef7386a2d354e64d2
Nora Granahan <namgranahan@gmail.com>
2018-03-22 18:35:02 -0400

    fixed order of push to firebase and append to DOM

--- script.js
+++ script.js
@@ -39,12 +39,30 @@ $(document).ready(function () {
         destination = $("#destinationInput").val().trim();
         trainStart = $("#trainStartInput").val().trim();
         frequency = $("#frequencyInput").val().trim();
-        console.log(trainName + ", " + destination + ", " + trainStart + ", " + frequency);
-        console.log("train start time: " + trainStart);
 
-        // TRAIN TIME CONVERSIONS
+        // PUSH TRAIN DATA TO DATABASE
+        dataRef.ref().push({
+
+            trainNameStored: trainName,
+            trainStartStored: trainStart,
+            destinationStored: destination,
+            frequencyStored: frequency,
+
+        })
+
+    })
+
+    // USING DATABASE REF, GRAB CHILD SNAPSHOT ON CHILD_ADDED TO GET EACH NEW TRAIN 
+    dataRef.ref().on("child_added", function (childSnapshot) {
+
+        trainName = childSnapshot.val().trainName;
+        trainStart = childSnapshot.val().trainStart;
+        destination = childSnapshot.val().destination;
+        frequency = childSnapshot.val().frequency;
+        
+        // TRAIN TIME CONVERSIONS -- NEED TO PUSH DATA TO DATABASE FIRST THEN DO THE CONVERSIONS WITH THE STORED DATA
         // First train time minus 1 year to make sure it's before current time
-        trainStartConvert = moment(trainStart, "HH:mm").subtract(1, "years");
+        trainStartConvert = moment(childSnapshot.val().trainStart, "HH:mm").subtract(1, "years");
         console.log("Converted train time: " + moment(trainStartConvert).format("hh:mm"));
 
         // Current time
@@ -64,38 +82,19 @@ $(document).ready(function () {
         console.log("Min until train: " + minAway);
 
         // Next to arrive
+        // This isn't working :( 
         nextTrain = moment().add(minAway, "minutes");
         nextTrainConvert = moment(nextTrain).format("mm:hh");
         console.log(nextTrain);
+        console.log(nextTrainConvert);
 
-        // PUSH TRAIN DATA TO DATABASE
-        dataRef.ref().push({
+        // APPEND EACH CHILD SNAPSHOT PROPERTY TO THE TABLE IN THE RIGHT COLUMN
+        var row = $("<tr>");
 
-            trainName: trainName,
-            destination: destination,
-            frequency: frequency,
-            nextTrain: nextTrainConvert,
-            minAway: minAway
+        row.append("<td>" + childSnapshot.val().trainName + "</td>" + "<td>" + childSnapshot.val().destination + "</td>" + "<td>" + childSnapshot.val().frequency + "</td>" + "<td>" + nextTrainConvert + "</td>" + "<td>" + minAway + "</td>")
 
-        })
-
+        $("tbody").append(row);
     })
-
-    // USING DATABASE REF, GRAB CHILD SNAPSHOT ON CHILD_ADDED TO GET EACH NEW TRAIN 
-dataRef.ref().on("child_added", function(childSnapshot) {
-    console.log(childSnapshot.val().trainName);
-    console.log(childSnapshot.val().destination);
-    console.log(childSnapshot.val().frequency);
-    console.log(moment(childSnapshot.val().nextTrainConvert).format("hh:mm A"));
-    console.log(childSnapshot.val().minAway);
-
-    // APPEND EACH CHILD SNAPSHOT PROPERTY TO THE TABLE IN THE RIGHT COLUMN
-    var row = $("<tr>");
-
-    row.append("<td>" + childSnapshot.val().trainName + "</td>" + "<td>" + childSnapshot.val().destination + "</td>" + "<td>" + childSnapshot.val().frequency + "</td>" + "<td>" + moment(childSnapshot.val().nextTrainConvert).format("hh:mm A") + "</td>" + "<td>" + childSnapshot.val().minAway + "</td>")
-
-    $("tbody").append(row);
-})
 
 });
 
